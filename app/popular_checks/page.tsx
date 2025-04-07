2425c927e79743c5d8bcd7ea8e34790c87579431
IykeMich <emperortigo001@gmail.com>
2025-04-07 19:01:56 +0100

    hello 2

--- app/popular_checks/page.tsx
+++ app/popular_checks/page.tsx
@@ -4,7 +4,10 @@ export default function PopularChecks() {
     return (
         <section className="pt-12 md:pt-16">
             <main className="py-12 md:px-12 lg:px-24">
+                <div className="px-4 md:px-0">
                 <PremiumChecks />
+
+                </div>
                 <div>
                     <PopularCars />
                 </div>
