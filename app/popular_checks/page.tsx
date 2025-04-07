0eda12890daabfbe593b60e971c91acdfff1f77c
IykeMich <emperortigo001@gmail.com>
2025-04-07 18:59:14 +0100

    hello

--- app/popular_checks/page.tsx
+++ app/popular_checks/page.tsx
@@ -3,7 +3,7 @@ import PopularCars from "../components/homepage/PopularCars"
 export default function PopularChecks() {
     return (
         <section className="pt-12 md:pt-16">
-            <main className="py-12 px-4 md:px-12 lg:px-24">
+            <main className="py-12 md:px-12 lg:px-24">
                 <PremiumChecks />
                 <div>
                     <PopularCars />
